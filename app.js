8835baaf4b0ca0a2c9414b8698317e53b7c5f13d
agentxx-code <agentxx0000@gmail.com>
2026-09-21 10:37:24 +0900

    fix: use Wix test-site report API

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const LIFF_ID = '2011674482-yvZEalkR';
-    const REPORT_API_URL = 'https://sitema0.wixstudio.com/my-site-1/_functions-dev/liffValuationReportData?siteRevision=2&branchId=eea00292-7df5-4eaa-b08f-835eb46d15bb';
+    const REPORT_API_URL = 'https://sitema0.wixstudio.com/my-site-1/_functions/liffValuationReportData?rc=test-site';
     const REPORT_REF_PATTERN = /^[a-f0-9]{32}$/;
     const statusElement = document.getElementById('status');
     const reportElement = document.getElementById('report');
